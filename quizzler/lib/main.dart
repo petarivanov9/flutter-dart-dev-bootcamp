cb7903b135ba0ee9e3d302001ec84fbab1442938
Petar Ivanov <pepincho95@gmail.com>
2020-01-09 17:29:29 +0200

    added initial questions and answers functionality

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -25,6 +25,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answers = [false, true, true];
+
+  int currentQuestionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[currentQuestionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +73,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctAnswer = answers[currentQuestionNumber];
+
+                if (correctAnswer) {
+                  print('... right ...');
+                } else {
+                  print('... wrong ...');
+                }
+
+                setState(() {
+                  currentQuestionNumber++;
+                });
               },
             ),
           ),
@@ -79,19 +101,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctAnswer = answers[currentQuestionNumber];
+
+                if (correctAnswer == false) {
+                  print('... right ...');
+                } else {
+                  print('... wrong ...');
+                }
+
+                setState(() {
+                  currentQuestionNumber++;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(),
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
