6cbdee195dd055804c9e85b3e5dfdaec7109f8a1
Petar Ivanov <pepincho95@gmail.com>
2020-01-27 20:01:07 +0200

    Add initial ResultsPage and navigation between screens

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,9 +1,11 @@
 import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'reusable_card.dart';
 import 'icon_content.dart';
+import 'results_page.dart';
 
 enum Gender {
   male,
@@ -216,11 +218,22 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return ResultsPage();
+                }),
+              );
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
